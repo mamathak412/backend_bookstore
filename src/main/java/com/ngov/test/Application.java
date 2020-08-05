d24077d0eb38ae6cd590d95a2d359988751d2d84
Mamatha <mamatha.k@al-enterprise.com>
2020-08-05 18:19:01 +0530

    updated

--- src/main/java/com/ngov/test/Application.java
+++ src/main/java/com/ngov/test/Application.java
@@ -1,13 +1,100 @@
 package com.ngov.test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.ByteArrayDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
 
 @SpringBootApplication
 public class Application {
 	
+	@Autowired
+    private KafkaProperties kafkaProperties;
+	
+	
 	public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+	
+	
+    @Bean
+    public Map<String, Object> consumerConfigs() {
+        Map<String, Object> props = new HashMap<>(
+                kafkaProperties.buildConsumerProperties()
+        );
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                JsonDeserializer.class);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG,
+                "twitter-groups");
+
+        return props;
+    }
+    
+    @Bean
+    public ConsumerFactory<String, Object> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(
+                kafkaProperties.buildConsumerProperties());
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+
+        return factory;
+    }
+    
+    // String Consumer Configuration
+
+    @Bean
+    public ConsumerFactory<String, String> stringConsumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(
+                kafkaProperties.buildConsumerProperties(), new StringDeserializer(), new StringDeserializer()
+        );
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerStringContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(stringConsumerFactory());
+
+        return factory;
+    }
+
+    // Byte Array Consumer Configuration
+
+    @Bean
+    public ConsumerFactory<String, byte[]> byteArrayConsumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(
+                kafkaProperties.buildConsumerProperties(), new StringDeserializer(), new ByteArrayDeserializer()
+        );
+    }
+    
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, byte[]> kafkaListenerByteArrayContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, byte[]> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(byteArrayConsumerFactory());
+        return factory;
+    }
+
 
 }
--- src/main/java/com/ngov/test/ws/TwitterRestServiceImpl.java
+++ src/main/java/com/ngov/test/ws/TwitterRestServiceImpl.java
@@ -1,36 +1,77 @@
 package com.ngov.test.ws;
 
-import java.io.IOException;
-import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+import java.util.stream.StreamSupport;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.header.Headers;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ngov.test.kafka.TwitterConsumer;
 import com.ngov.test.model.Twitter;
 
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 @RestController
 public class TwitterRestServiceImpl {
-	
-	@Autowired
-	private TwitterConsumer twitterConsumer;
-	
-	@CrossOrigin(origins = "http://135.254.163.44:4200")
-	@RequestMapping(value="/twitters", method=RequestMethod.GET)
-	public ResponseEntity<List<Twitter>> twitters() throws IOException {
-		List<Twitter> twitters = twitterConsumer.consume();
-		return new ResponseEntity<List<Twitter>>(twitters, HttpStatus.OK);
+
+	private static final Logger logger = LoggerFactory.getLogger(TwitterRestServiceImpl.class);
+
+	private final KafkaTemplate<String, Object> template;
+	private final String topicName = "twitter";
+	private final int messagesPerRequest = 10;
+	private CountDownLatch latch;
+
+	public TwitterRestServiceImpl(final KafkaTemplate<String, Object> template) {
+		this.template = template;
+	}
+
+	@GetMapping("/twitters")
+	public String twitters() throws Exception {
+		latch = new CountDownLatch(messagesPerRequest);
+		IntStream.range(0, messagesPerRequest)
+				.forEach(i -> this.template.send(topicName, String.valueOf(i), new Twitter()));
+		latch.await(60, TimeUnit.SECONDS);
+		logger.info("All messages received");
+		return "Hello Kafka!";
+	}
+
+	@KafkaListener(topics = "twitter", containerFactory = "kafkaListenerContainerFactory")
+	public void listenAsObject(ConsumerRecord<String, Twitter> cr, @Payload Twitter payload) {
+		logger.info("Logger 1 [JSON] received key {}: Type [{}] | Payload: {} | Record: {}", cr.key(),
+				typeIdHeader(cr.headers()), payload, cr.toString());
+		latch.countDown();
+	}
+
+	@KafkaListener(topics = "twitter", containerFactory = "kafkaListenerStringContainerFactory")
+	public void listenasString(ConsumerRecord<String, String> cr, @Payload String payload) {
+		logger.info("Logger 2 [String] received key {}: Type [{}] | Payload: {} | Record: {}", cr.key(),
+				typeIdHeader(cr.headers()), payload, cr.toString());
+		latch.countDown();
+	}
+
+	@KafkaListener(topics = "twitter", containerFactory = "kafkaListenerByteArrayContainerFactory")
+	public void listenAsByteArray(ConsumerRecord<String, byte[]> cr, @Payload byte[] payload) {
+		logger.info("Logger 3 [ByteArray] received key {}: Type [{}] | Payload: {} | Record: {}", cr.key(),
+				typeIdHeader(cr.headers()), payload, cr.toString());
+		latch.countDown();
+	}
+
+	private static String typeIdHeader(Headers headers) {
+		return StreamSupport.stream(headers.spliterator(), false).filter(header -> header.key().equals("__TypeId__"))
+				.findFirst().map(header -> new String(header.value())).orElse("N/A");
 	}
 
 }
